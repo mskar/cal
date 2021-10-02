8ed82a78f076fdeaeec32105f11b216d6a09b3bf
Martin Skarzynski <marskar@gmail.com>
2021-10-01 21:46:15 -0400

    fix week dates, add a 2nd notes cell, center the table

--- cal.js
+++ cal.js
@@ -1,12 +1,12 @@
-var table = document.getElementById('myTable')
-var myDate = new Date();
-myDate.setDate(myDate.getDate() - 1)
+let table = document.getElementById('myTable');
+const leftDate = new Date(document.getElementById("leftDate").value);
+const rightDate = new Date(document.getElementById("rightDate").value);
 
 function ISO8601_week_no(dt) {
-    var tdt = new Date(dt.valueOf());
-    var dayn = (dt.getDay() + 6) % 7;
+    let tdt = new Date(dt.valueOf());
+    let dayn = (dt.getDay() + 7) % 7;
     tdt.setDate(tdt.getDate() - dayn + 3);
-    var firstThursday = tdt.valueOf();
+    let firstThursday = tdt.valueOf();
     tdt.setMonth(0, 1);
     if (tdt.getDay() !== 4) {
         tdt.setMonth(0, 1 + ((4 - tdt.getDay()) + 7) % 7);
@@ -14,24 +14,40 @@ function ISO8601_week_no(dt) {
     return 1 + Math.ceil((firstThursday - tdt) / 604800000);
 }
 
-for (var i = 0; i < 16; i++) {
-    var row = document.createElement('TR');
-    var cell1 = document.createElement('TD');
-    var cell2 = document.createElement('TD');
-    var cell3 = document.createElement('TD');
-    var isoDate = myDate.toISOString().substring(0, 10)
-    var weekDate = myDate.getFullYear() + "-W" + ISO8601_week_no(myDate) + "-" + (myDate.getDay() + 1)
-    cell1.innerHTML = isoDate + "<br>" + weekDate;
-    cell3.innerHTML = isoDate + "<br>" + weekDate;
+function formatDates(dt) {
+    const isoDate = dt.toISOString().substring(0, 10);
+    const weekDate = `${
+        dt.getFullYear()
+    }-W${
+        ISO8601_week_no(dt)
+    }-${
+        (dt.getDay() + 1)
+    }`;
+    return isoDate + "<br>" + weekDate;
+}
+
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement('TR');
+    let cell1 = document.createElement('TD');
+    let cell2 = document.createElement('TD');
+    let cell3 = document.createElement('TD');
+    let cell4 = document.createElement('TD');
+    cell1.innerHTML = formatDates(leftDate);
     cell2.innerHTML = "<hr>";
+    cell3.innerHTML = "<hr>";
+    cell4.innerHTML = formatDates(rightDate);
     cell1.setAttribute("class", "cell");
-    cell3.setAttribute("class", "cell");
     cell2.setAttribute("class", "cell");
-    cell2.setAttribute("width", "79%");
+    cell2.setAttribute("width", "39%");
+    cell3.setAttribute("width", "39%");
+    cell3.setAttribute("class", "cell");
+    cell4.setAttribute("class", "cell");
     row.appendChild(cell1);
     row.appendChild(cell2);
     row.appendChild(cell3);
+    row.appendChild(cell4);
     table.tBodies[0].appendChild(row);
-    myDate.setDate(myDate.getDate() + 1);
+    leftDate.setDate(leftDate.getDate() + 1);
+    rightDate.setDate(rightDate.getDate() + 1);
 }
 console.log()
