36a9d3f3500070444e9b965f6c5113fbd352b5d9
Martin Skarzynski <13444106+mskar@users.noreply.github.com>
2023-01-22 20:12:13 -0500

    refactor and add second output type

--- cal.py
+++ cal.py
@@ -1,22 +1,47 @@
-import re
-import pathlib
+import calendar
 import datetime
+import itertools
+import pathlib
+import re
 
 
 class Calendar:
-    def __init__(self, start_date: str = "1970-01-01", n_days: int = 7):
+    def __init__(self, start_date: str = "1970-01-01", n_days: int = 365):
         self.dates = [
             datetime.date.fromisoformat(start_date) + datetime.timedelta(days=d)
             for d in range(n_days)
         ]
-        self.name = f"{self.dates[0].isoformat()}_{self.dates[-1].isoformat()}"
-        self.head = (
+        self.year_months = sorted(list(set((d.year, d.month) for d in self.dates)))
+        self.month_dates = sorted(
+            list(
+                set(
+                    d
+                    for m in [
+                        list(calendar.Calendar().itermonthdates(*ym))
+                        for ym in self.year_months
+                    ]
+                    for d in m
+                )
+            )
+        )
+        self.weeks = list(zip(*[iter(self.month_dates)] * 7))
+        self.__head = (
             "<!DOCTYPE html>\n<html>\n\n<head>\n\t"
             '<link rel="stylesheet" href="cal.css">\n'
             "</head>\n\n<body>\n\t<main>\n\t\t"
         )
-        self.foot = "\t</main>\n</body>\n\n</html>"
-        self.body = "".join([
+        self.__foot = "\t</main>\n</body>\n\n</html>"
+
+    def __str__(self):
+        return self.name.replace("_", " to ")
+
+    def __repr__(self):
+        return f"Calendar(start_date={self.dates[0]}, n_days={len(self.dates)})"
+
+    def write_times(self):
+        for week in self.weeks:
+            html = "".join(
+                [
                     "<day>\n\t\t\t"
                     f"<date>{d.isoformat()}&nbsp&nbsp;</date>\n\t\t\t"
                     "<blank></blank>\n\t\t\t"
@@ -37,20 +62,33 @@ class Calendar:
                     "<time>750</time>\n\t\t\t"
                     "<dashed></dashed>\n\t\t\t"
                     "<time>875</time>\n\t\t</day>\n\t\t"
-                    for d in self.dates
-                ])
-        self.html = self.head + self.body + self.foot
+                    for d in week
+                ]
+            )
+            pathlib.Path(
+                f"{week[0].isoformat()}_{week[-1].isoformat()}.html"
+            ).write_text(self.__head + html + self.__foot)
+        return self
 
-    def __str__(self):
-        return self.name.replace("_", " to ")
-
-    def __repr__(self):
-        return f"Calendar(start_date={self.dates[0]}, n_days={len(self.dates)})"
-
-    def write_file(self, filename: str = None):
-        if not filename:
-            filename = self.name + ".html"
-        pathlib.Path(filename).write_text(self.html)
+    def write_dates(self):
+        for eight_weeks in zip(*[iter(self.weeks)] * 8):
+            days = [d for w in eight_weeks for d in w]
+            html = (
+                "<dates>\n\t\t\t"
+                + "".join(
+                    [
+                        f"<date>{d.isoformat()}</date>\n\t\t\t"
+                        f"<date>{d.strftime('%Y-W%V-%u')}</date>\n\t\t\t"
+                        f"<date>{d.strftime('%Y-%j')}</date>\n\t\t\t"
+                        f"{'<solid></solid>' if i % 2 else '<dashed></dashed>'}\n\t\t\t"
+                        for (i, d) in enumerate(days)
+                    ]
+                )
+                + "</dates>\n\t\t"
+            )
+            pathlib.Path(
+                f"{days[0].isoformat()}_{days[-1].isoformat()}.html"
+            ).write_text(self.__head + html + self.__foot)
         return self
 
     @staticmethod
@@ -81,12 +119,15 @@ class Calendar:
 
 
 if __name__ == "__main__":
+    cal = Calendar("2023-01-23")
+    cal.year_months
+    cal.write_times()
+    cal.write_dates()
+    len(list(zip(*cal.monthdates)))
+    cal.monthdates
     # Generate html files for each week in 2023
     start = datetime.date.fromisoformat("2023-01-23")
-    dates = [start] + [
-        start + datetime.timedelta(days=7*n)
-        for n in range(50)
-    ]
+    dates = [start] + [start + datetime.timedelta(days=7 * n) for n in range(50)]
     dates
     cals = [Calendar(d.isoformat()).write_file() for d in dates]
     cals
